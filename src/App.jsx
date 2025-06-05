ff3028ac5b3673490adb5c564a7149724a646888
Veikka <veikka173@gmail.com>
2025-06-05 16:47:44 +0300

    Synch with vscode

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TerminalComponent from "./components/Terminal";
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <TerminalComponent />
+    </div>
+  );
+}
+
+export default App;
